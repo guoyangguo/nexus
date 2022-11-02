d5ae9114eee931cc5f420ed486936090035f5318
guoyang <596840946@qq.com>
2022-11-02 22:34:19 +0800

    add maven-nexus-repo-demo

---
+++ maven-nexus-repo-demo/maven-nexus-repo-demo/src/main/java/com/autumnin/MavenNexusRepoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.autumnin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenNexusRepoDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenNexusRepoDemoApplication.class, args);
+    }
+
+}
